8d423b7cced2e2af245e8df01c3a684e93f3bfb3
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2023-12-14 16:31:09 +0200

    task-4 was done

--- js/task-03.js
+++ js/task-03.js
@@ -1,11 +1,13 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', event => {
+const updateValue = event => {
   const outputValue = event.currentTarget.value.trim();
   if (outputValue === '' || outputValue === ' ') {
     output.textContent = 'Anonymous';
   } else {
     output.textContent = outputValue;
   }
-});
+};
+
+input.addEventListener('input', updateValue);
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', checkFormSubmission);
+
+function checkFormSubmission(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  const loginFormValues = {
+    email: email.trim(),
+    password: password.trim(),
+  };
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(loginFormValues);
+    form.reset();
+  }
+}
