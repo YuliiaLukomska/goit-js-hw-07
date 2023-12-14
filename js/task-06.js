2e7a8604b7e38d2f45f1a4652da730bebd1d4992
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2023-12-14 21:08:01 +0200

    first part of task 6 was done

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function checkInput(event) {
+  const inputValue = event.currentTarget.value;
+  if (inputValue >= 1 && inputValue <= 100) {
+    createBtn.addEventListener('click', createBoxes(inputValue));
+    input.value = '';
+    createBtn.addEventListener('click', () => {
+      boxes.innerHTML = '';
+      createBoxes(inputValue);
+    });
+  } else {
+    return;
+  }
+}
+
+input.addEventListener('change', checkInput);
+
+destroyBtn.addEventListener('click', () => {});
+
+function createBoxes(amount) {
+  const markupArray = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const div = '<div></div>';
+    markupArray[i] = div;
+  }
+  const markup = markupArray.join('');
+  boxes.insertAdjacentHTML('afterbegin', markup);
+}
+
+// const result = createBoxes(5);
+// console.log(result);
